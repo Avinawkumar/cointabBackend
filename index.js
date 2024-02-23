4d427f55a2b1cf860f9e3050f7827971e8c0c6b9
Avinaw Kumar <0811kumaravinaw@gmail.com>
2024-02-23 19:52:04 +0530

    issue fixed

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const app =  express()
 app.use(express.json());
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 // Basic routes
 app.get("/",(req,res)=>{
     res.send("hello from cointab assigntment backend!");
